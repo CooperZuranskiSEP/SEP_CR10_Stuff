7b02f4413f4a2b738a2a62114efe04b3ae53345c
cmzuranski <cmzuranski@sep.com>
2023-11-05 23:06:48 -0500

    lin advance base value

--- Marlin-2.1.2.1/Marlin/Configuration_adv.h
+++ Marlin-2.1.2.1/Marlin/Configuration_adv.h
@@ -2113,7 +2113,7 @@
   #if ENABLED(DISTINCT_E_FACTORS)
     #define ADVANCE_K { 0.0 }     // (mm) Compression length per 1mm/s extruder speed, per extruder
   #else
-    #define ADVANCE_K 0.0         // (mm) Compression length applying to all extruders
+    #define ADVANCE_K 0.5         // (mm) Compression length applying to all extruders
   #endif
   //#define ADVANCE_K_EXTRA       // Add a second linear advance constant, configurable with M900 L.
   //#define LA_DEBUG              // Print debug information to serial during operation. Disable for production use.
