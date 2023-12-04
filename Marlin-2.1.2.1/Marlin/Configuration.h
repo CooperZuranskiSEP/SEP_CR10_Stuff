9eae55e8840cf482e715b0b0ee720fb13a6747b3
cmzuranski <cmzuranski@sep.com>
2023-12-04 14:15:29 -0500

    z offset more focused on stick than shine

--- Marlin-2.1.2.1/Marlin/Configuration.h
+++ Marlin-2.1.2.1/Marlin/Configuration.h
@@ -1484,7 +1484,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -41.7, -14.5, -1.984 }
+#define NOZZLE_TO_PROBE_OFFSET { -41.7, -14.5, -2.045 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
